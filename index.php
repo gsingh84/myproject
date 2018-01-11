905a382fda79f62a955da8b3de73f292c6344c14
gsingh84 <gsingh84@mail.greenriver.edu>
2018-01-11 16:45:37 -0500

    added paragraph

--- index.php
+++ index.php
@@ -1,2 +1,4 @@
 <h1>Hello</h1>
 <h2>How are you?</h2>
+
+<p>Text messaging, or texting, is the act of composing and sending electronic messages, typically consisting of alphabetic and numeric characters, between two or more users of mobile phones, tablets, desktops/laptops, or other devices.</p>
